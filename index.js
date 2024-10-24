359df57d47dd23245ed038ed64e7124cc6417f73
abdomo7sen <abdomo7sen20006@gmail.com>
2024-10-24 08:29:22 +0300

    update

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ import { AppError } from "./src/utils/appError.js";
 import { globalError } from "./src/middleware/globalError.js";
 const app = express();
 const port = process.env.PORT||3000;
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/auth", userRouter);
 app.use("/notes", noteRouter);
